91b339258b118dc2dabc2a53d420e46080b80fbf
Worrah <34601253+Worrah@users.noreply.github.com>
2025-12-07 22:45:16 +0300

    stuff

--- modules/handlers.py
+++ modules/handlers.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from modules.logger import info,warn,error
-from modules.handlers import *
+from modules.utils import *
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой бот.")
